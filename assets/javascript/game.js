a5c09a23cb559c9ae6ac7ad4dc2cb4010783c416
Maggie <test@apples-MBP.attlocal.net>
2017-03-01 23:34:34 -0600

    steps 1 - 3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,64 @@
 // 1. start button
 	// 1.1 when start button is clicked show trivia game
-// 2.  timer begins when start button is clicked at 30sec.
+
+	// 	Add an on click listener to all elements that have the class .startBtn 
+
+		$('.startBtn').on('click', function (makeTriviaAndTimerScreenAppear) {
+			console.log ('start buttonClicked!');	
+			$('.starterScreen').css({
+				'display': 'none'
+			});
+			$('.triviaScreen').css({
+				'display': 'block'
+			});
+			$('.timerScreen').css({
+				'display': 'block'
+			});
+		}); 
+
+
+// 2.  timer (5 sec) begins when start button is clicked
+// 	
+		$('.startBtn').on('click', function (startTimerGoing) {
+			var n = 5;
+			setTimeout(countDown, 1000);
+
+			function countDown(){
+			   n--;
+			   if(n > 0){
+			      setTimeout(countDown,1000);
+			   }
+			   $(".timerScreen").html('<h2> Time Left: ' + n + ' Seconds!</h2>');
+			}
+		});
+
 // 3. user clicks an answer for each question
+	$('.btnA').on('click', function (storeTriviaBtnAnswer) {
+			console.log ('Trivia Button A Clicked!');	
+		}); 
+
 // 4. store information from answer 
 	// 4.1 compare to correct answer
-// 5. add value to win, lose or unanswered
-// 6. when timer equals 0 end game
-// 7. restart (empty values of trivia answers)
+
+// 5. add value of 4.1 as win, lose or unanswered
+
+// 6. end game
+	// 6.1 when timer equals 0 
+	// 6.2 OR user answers last question
+
+// 7. restart 
+		// 7.1 (empty values of trivia answers)
+		// 7.2 return to start screen
+
+		$('.restartBtn').on('click', function () {
+			console.log ('restart buttonClicked!');	
+			$('.starterScreen').css({
+				'display': 'block'
+			});
+			$('.endScreen').css({
+				'display': 'none'
+			});
+		}); 
 
 console.log ("hi maggie");
 
@@ -30,7 +82,7 @@ console.log ("hi maggie");
 
 	// 	Javascript
 
-	// 	document.onkeyup = function(evnt) {
+	// 	document.onkeyup = function(event) {
 
 	// 	}
 
