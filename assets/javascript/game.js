cb18197fec1ce761512b8774eaca33384d2ec673
Maggie <test@apples-MacBook-Pro.local>
2017-03-01 21:30:24 -0600

    create files

---
+++ assets/javascript/game.js
@@ -0,0 +1,95 @@
+// 1. start button
+	// 1.1 when start button is clicked show trivia game
+// 2.  timer begins when start button is clicked at 30sec.
+// 3. user clicks an answer for each question
+// 4. store information from answer 
+	// 4.1 compare to correct answer
+// 5. add value to win, lose or unanswered
+// 6. when timer equals 0 end game
+// 7. restart (empty values of trivia answers)
+
+console.log ("hi maggie");
+
+// APPLICATION STATE
+	// variable list 
+		// var firstNumber = '';
+		// var secondNumber = '';
+		// var result = null;
+		// var phase = 'start';
+
+	// store data in object
+
+		// appState = {
+		// 	firstNumber: '',
+		// 	secondNumber: '',
+		// 	result: null,
+		// phase: 'start'
+		// }
+
+// EVENT MANAGEMENT
+
+	// 	Javascript
+
+	// 	document.onkeyup = function(evnt) {
+
+	// 	}
+
+	// ****************
+
+	// 	JQuery
+
+	// 	Add an on click listener to all elements that have the class buttonAnswer 
+
+	// 	$('.buttonAnswer').on('click', function () {
+
+	// 		var buttonValue = this.value;
+	// 		handleAnswerSelection (buttonValue);	
+	// 	}); 
+
+// FUNCTIONS AND LOGIC
+
+
+	// process answer selected by user
+
+		// function handleAnswerSelection(buttonValue) {
+
+
+		// }
+
+	// if/else: compare user answer to actual answer
+
+		// if (condition1){
+		// 	block of code to be executed if the condition1 is true
+		// } else if (condition2) {
+		// 	block of code to be executed if the condition1 is false and condition2 is true
+		// } else {
+		// 	block of code to be executed if the condition 1 is false and condition 2 is false
+		// }
+
+
+// DISPLAY MANAGEMENT 
+
+// 	function that pushes appState to the display 
+
+	// 	function refreshDisplay(){
+	// 		console.log(appState);
+
+	// 	}
+
+// // INITIALIZE / SETUP APPLICATION
+
+// // 	Use a function to initialize trivia
+
+		// 	function initializeTrivia(){
+		// 		//reset the appState values
+
+		// 		appState.firstNumber = ''
+		// 		appState.secondNumber = ''
+		// 		appState.result = null
+		// 		appState.result = 'start'
+				
+		// 		// clear display
+
+		// 		refreshDisplay();
+		// 	}
+
