583bdf4deb780d050c0c335d2c450b6028bf7566
Maggie <test@apples-MBP.attlocal.net>
2017-03-02 14:10:40 -0600

    change buttons to form

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,7 +18,7 @@
 
 
 // 2.  timer (5 sec) begins when start button is clicked
-// 	
+// 		
 		$('.startBtn').on('click', function (startTimerGoing) {
 			var n = 5;
 			setTimeout(countDown, 1000);
@@ -29,32 +29,104 @@
 			      setTimeout(countDown,1000);
 			   }
 			   $(".timerScreen").html('<h2> Time Left: ' + n + ' Seconds!</h2>');
+				
+// QUESTION: I'M HAVING A SCOPE PROBLEM BECAUSE I CANNOT GET THE ALERTGAMEOVER TO WORK OUTSIDE OF THE COUNTDOWN FUNCTION
+// WHICH IS MAKING THIS MORE COMPLICATED THAN NECESSARY.  THOUGHTS?
+
+				function alertGameOver(){
+					if(n === 0){
+						$(".timerScreen").html('<h2> Time is up!</h2>');
+				
+					// add values to scoreboard
+					compareAnswerValues()
+
+					// make trivia disapear
+						$('.triviaScreen').css({
+						'display': 'none'
+						});
+
+					// make .endScreen appear
+						$('.endScreen').css({
+						'display': 'block'
+						});
+					}
+				}
+			
+			alertGameOver();
+
 			}
+
+			
 		});
 
-// 3. user clicks an answer for each question
+// 3. variable for wins, losses and unanswered
+
+var win = 0;
+var loss = 0;
+var unanswered = 0;
+
+
+
+	// 3.0 Variable for Correct Answers
+var correctAnswer1 = "Honda";
+	
+	// 3.2 store information from answer 
+
+var userAnswer1
+
+// 4. User's answer for each question is stored in userAnswer1 variable when Submit Button is Clicked
+
+// QUESTION: I CAN ONLY DO THIS ON WHEN THE SUBMIT BUTTON IS PUSHED.  IS THERE ANOTHER WAY TO GRAB THE DATA VALUE?
+
 	$('.btnA').on('click', function (storeTriviaBtnAnswer) {
-			console.log ('Trivia Button A Clicked!');	
+			console.log ('Trivia Button Clicked!');	
+			userAnswer1 = $('input[name="question1"]:checked').val();
+			console.log(userAnswer1);
+			compareAnswerValues();
 		}); 
+	
+// 5. compare correctAnswer1 to userAnswer1
+		function compareAnswerValues() {
+			i = 0;
+			if (userAnswer1 === undefined) {
+					unanswered = i + 1;
+					console.log ("unanswered:" + unanswered);
+				} else if (userAnswer1 === correctAnswer1){
+					win = i + 1;
+					console.log ("win:" + win);
+				} else {
+					loss = i + 1;
+					console.log ("losses:" + win);
+				}
+				showScoreboard();
+			}
+				
 
-// 4. store information from answer 
-	// 4.1 compare to correct answer
+	// 5.1 add value of 4.1 as win, lose or unanswered
+		function showScoreboard() {
+			$(".scoreBoard").html('<h2>All Done!</h2><h3>Wins: ' + win + '</h3> <h3>Losses: ' + loss + '</h3>' + '<h3>Unanswerd: ' + unanswered + '</h3>');
+		}	
 
-// 5. add value of 4.1 as win, lose or unanswered
+// 6. end game [submit all values]
+	// 6.1 when setTimeOut = 0
+ 
+	// 6.2 OR user submits answers [I think the instructions mean for it to happen automatically when the last question is answered]
 
-// 6. end game
-	// 6.1 when timer equals 0 
-	// 6.2 OR user answers last question
 
 // 7. restart 
 		// 7.1 (empty values of trivia answers)
-		// 7.2 return to start screen
+		// 7.2 return to start landing 
 
 		$('.restartBtn').on('click', function () {
 			console.log ('restart buttonClicked!');	
 			$('.starterScreen').css({
 				'display': 'block'
 			});
+
+			$('.timerScreen').css({
+				'display': 'none'
+			});
+			
 			$('.endScreen').css({
 				'display': 'none'
 			});
